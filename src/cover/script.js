401826ae24236ea9a09281a55bc0d41c61ded561
Jaime Simeón Palomar Blumenthal <alu0101228587@ull.edu.es>
2023-10-19 09:51:48 +0100

    Added random quote section

---
+++ src/cover/script.js
@@ -0,0 +1,26 @@
+// Imports
+import axios from "axios";
+
+// Document elements
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+// Global function calls
+getRandomQuote();
+
+// Function declarations
+function getRandomQuote() {
+  axios.get("https://finalspaceapi.com/api/v0/quote/").then((response) => {
+    console.log(response.data);
+
+    const random = Math.floor(Math.random() * (response.data.length + 1));
+
+    const randomQuote = response.data[random].quote;
+    const randomQuoteAuthor = response.data[random].by;
+
+    quote.innerText = `"${randomQuote}"`;
+    author.innerText = `${randomQuoteAuthor}`;
+  });
+}
+
+// Event listeners
