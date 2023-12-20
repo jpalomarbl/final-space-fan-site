8fba76714e263c8054268eb583ec4bed1bb1a929
Jaime Simeón Palomar Blumenthal <alu0101228587@ull.edu.es>
2023-12-20 20:03:15 +0000

    Finalizado fondo

--- src/category/script.js
+++ src/category/script.js
@@ -16,6 +16,8 @@ let episodesAmount = 0;
 
 let promises = [];
 
+starGenerator();
+
 promises.push(
   new Promise((resolve, reject) => {
     axios.get("https://finalspaceapi.com/api/v0/location/").then((response) => {
@@ -184,4 +186,41 @@ Promise.all(promises).then(() => {
       }
     });
   });
+});
+
+function starGenerator() {
+  let scrollHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  );
+
+  for (let i = 0; i < 300; i++) {
+    let star = document.createElement("div");
+    star.className = "star";
+    star.style.top = Math.random() * scrollHeight + "px";
+    star.style.left = Math.random() * window.innerWidth + "px";
+    star.style.animationDuration =
+      Math.floor(Math.random() * (15 - 7 + 1)) + "s";
+    star.style.animationDelay = Math.random() * 2 + "s";
+    document.body.appendChild(star);
+  }
+}
+
+//Listeners
+let resizeTimer;
+window.addEventListener("resize", function (e) {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function () {
+    let stars = document.getElementsByClassName("star");
+
+    while (stars.length > 0) {
+      stars[0].parentNode.removeChild(stars[0]);
+    }
+
+    starGenerator();
+  }, 250);
 });
