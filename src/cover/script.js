1da4b10ae5b283b191b37012cf44f295f5e9afbd
Jaime Simeón Palomar Blumenthal <alu0101228587@ull.edu.es>
2023-12-20 19:43:21 +0000

    Added footer and background for index

--- src/cover/script.js
+++ src/cover/script.js
@@ -7,6 +7,7 @@ const author = document.getElementById("author");
 
 // Global function calls
 getRandomQuote();
+starGenerator();
 
 // Function declarations
 function getRandomQuote() {
@@ -23,4 +24,40 @@ function getRandomQuote() {
   });
 }
 
-// Event listeners
+function starGenerator() {
+  let scrollHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  );
+
+  for (let i = 0; i < 300; i++) {
+    let star = document.createElement("div");
+    star.className = "star";
+    star.style.top = Math.random() * scrollHeight + "px";
+    star.style.left = Math.random() * window.innerWidth + "px";
+    star.style.animationDuration =
+      Math.floor(Math.random() * (15 - 7 + 1)) + "s";
+    star.style.animationDelay = Math.random() * 2 + "s";
+    document.body.appendChild(star);
+  }
+}
+
+//Listeners
+let resizeTimer;
+window.addEventListener("resize", function (e) {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function () {
+    let stars = document.getElementsByClassName("star");
+
+    while (stars.length > 0) {
+      stars[0].parentNode.removeChild(stars[0]);
+    }
+
+    starGenerator();
+  }, 250);
+});
+
